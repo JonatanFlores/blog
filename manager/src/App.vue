25b0d0ccdda87ecb9f4e24bab7de6a54dc2adb9b
Jonatan Flores <jonatafloress@gmail.com>
2020-01-31 13:13:06 -0300

    Manager setup

---
+++ manager/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="app">
+    <div id="nav"><router-link :to="{ name: 'dashboard' }">Dashboard</router-link></div>
+    <router-view />
+  </div>
+</template>
---
+++ manager/src/views/Dashboard/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="dashboard">
+    <h1>Dashboard</h1>
+    <img src="@/assets/images/logo.png" alt="Manager Logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard',
+  components: {},
+};
+</script>
+
+<style lang="scss">
+#dashboard {
+}
+</style>
